8ee38b08a0749cbe57635886ffeb9ac97a152de7
Tim Alexander Söther <TAS@p200300794f193c37a2999bfffe07ad09.dip0.t-ipconnect.de>
2016-05-21 17:11:49 +0200

    Initial Commit

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Tim Alexander Söther on 21.05.16.
+//  Copyright © 2016 Tim Alexander Söther. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
